50eb2fe2902e4cb495a496405014b9b58fa51ef3
Sriram Iyer <sriram.iyer@hpinc.com>
2023-08-29 18:50:11 +0530

    cleanup v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
-			path: '/',
-			name: 'home',
-			component: HomeView,
-		},
-		{
 			path: '/about',
+			alias: '/',
 			name: 'about',
 			// route level code-splitting
 			// this generates a separate chunk (About.[hash].js) for this route
